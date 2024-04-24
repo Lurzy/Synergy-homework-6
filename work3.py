d7d04ca6016517a990d09a93218628215099721b
Lurzy <purpleswtf@gmail.com>
2024-04-24 23:33:39 +0300

    Добавлены две задачи

--- work3.py
+++ work3.py
@@ -4,7 +4,7 @@ b = int(input())
 if a > b:
     print("Ошибка, значение A > B")
 else:
-    for i in range (a, b+1, +1):
+    for i in range (a, b+1):
         if i % 2 == 0:
             print (i)
 
